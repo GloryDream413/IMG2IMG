5d55b29b05035934ab00a4d9b4ade039e54adea9
GloryDream413 <putnikdeveloperbg@gmail.com>
2023-05-21 21:54:30 +0900

    Upload Function Changed.

--- frontend/src/components/dream/Dream.jsx
+++ frontend/src/components/dream/Dream.jsx
@@ -2,32 +2,40 @@ import React, { useCallback, useState } from 'react';
 import './dream.css';
 import { useDropzone } from 'react-dropzone';
 import { Link } from 'react-router-dom';
-
-function UploadDropZone() {
-  const onDrop = useCallback((acceptedFiles) => {
-    // Handle the uploaded files here
-    console.log(acceptedFiles);
-  }, []);
-
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
-
-  return (
-    <div className="upload-container">
-      <div {...getRootProps()} className={`box-container ${isDragActive ? 'active' : ''}`}>
-        <input {...getInputProps()} />
-        <p>{isDragActive ? 'Drop the files here' : 'Drag and drop files here'}</p>
-      </div>
-    </div>
-  );
-}
-
 export const Dream = () => {
-
+  const [pictureRoute, setPictureRoute] = useState('')
   const [prompt, setPrompt] = useState('')
   const onPromptChange = (event) => {
     setPrompt(event.target.value);
   };
+  
+  const onDrop = useCallback((acceptedFiles) => {
+    // Handle the uploaded files here
+    console.log(acceptedFiles[0]);
+    const file = acceptedFiles[0];
+    const imageUrl = URL.createObjectURL(file);
+    const image = new Image();
+    // Set the image source to the URL
+    image.src = imageUrl;
 
+    // Wait for the image to load
+    image.onload = function() {
+      // Create a canvas element
+      const canvas = document.createElement('canvas');
+      // Set the canvas dimensions to match the image dimensions
+      canvas.width = image.width;
+      canvas.height = image.height;
+      // Draw the image on the canvas
+      const ctx = canvas.getContext('2d');
+      ctx.drawImage(image, 0, 0);
+      // Get the data URL for the canvas
+      const dataUrl = canvas.toDataURL('image/png');
+      setPictureRoute(dataUrl);
+    };
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+  
   return (
     <div className="gpt3__header section__padding" id="home">
       <div className='generate'>
@@ -39,7 +47,12 @@ export const Dream = () => {
       <div className="gpt3__header-content">
         <div className="original">
           <h2>Upload Image</h2>
-          <UploadDropZone />
+          <div className="upload-container">
+            <div {...getRootProps()} className={`box-container ${isDragActive ? 'active' : ''}`}>
+              <input {...getInputProps()} />
+              <p>{isDragActive ? 'Drop the file here' : 'Drag and drop file here'}</p>
+            </div>
+          </div>
           <h2>Input Prompt</h2>
           <textarea
             className="desc"
