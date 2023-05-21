a2dbe46cfbd431b456bf850fb7754173cda70824
GloryDream413 <putnikdeveloperbg@gmail.com>
2023-05-21 23:24:27 +0900

    Image uploading function added.

--- frontend/src/components/dream/Dream.jsx
+++ frontend/src/components/dream/Dream.jsx
@@ -1,14 +1,43 @@
-import React, { useCallback, useState } from 'react';
+import React, { useEffect, useCallback, useState } from 'react';
 import './dream.css';
 import { useDropzone } from 'react-dropzone';
-import { Link } from 'react-router-dom';
+import axios from 'axios'
+import ClipLoader from "react-spinners/ClipLoader";
+import image_mask from '../../assets/mask_image.png'
+
 export const Dream = () => {
   const [pictureRoute, setPictureRoute] = useState('')
+  const [maskRoute, setMaskRoute] = useState('')
+  const [nftRoute, setNftRoute] = useState('')
   const [prompt, setPrompt] = useState('')
   const onPromptChange = (event) => {
     setPrompt(event.target.value);
   };
   
+  useEffect(() => {
+    const image = new Image();
+    // Set the image source to the URL
+    image.src = image_mask;
+    // Wait for the image to load
+    image.onload = function() {
+      // Create a canvas element
+      const canvas = document.createElement('canvas');
+
+      // Set the canvas dimensions to match the image dimensions
+      canvas.width = image.width;
+      canvas.height = image.height;
+
+      // Draw the image on the canvas
+      const ctx = canvas.getContext('2d');
+      ctx.drawImage(image, 0, 0);
+
+      // Get the data URL for the canvas
+      const dataUrl = canvas.toDataURL('image/png');
+      setMaskRoute(dataUrl);
+      // dataUrl is the URI-based data URL for the image in PNG format
+    };
+  }, []);
+
   const onDrop = useCallback((acceptedFiles) => {
     // Handle the uploaded files here
     console.log(acceptedFiles[0]);
@@ -35,14 +64,35 @@ export const Dream = () => {
   }, []);
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
-  
+  const [bLoadingFlag, setLoadingFlag] = useState(false)
+
+  const onGenerate = async () => {
+    setLoadingFlag(true);
+    const response = await axios.post(
+      'http://localhost:7777/getImage',
+      {
+        image_original : pictureRoute,
+        prompt : prompt,
+        image_mask : maskRoute
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    )
+    setNftRoute(response.data.response.output[0]);
+    console.log("NFT-Route", nftRoute);
+    setLoadingFlag(false);
+  };
+
   return (
     <div className="gpt3__header section__padding" id="home">
       <div className='generate'>
         <h1><b>Generating dream rooms <span>using AI</span> for everyone.</b></h1>
       </div>
       <div className='generate'>
-        <button type="button" ><Link to="/dream">Redesign your image</Link></button>
+        <button type="button" onClick={onGenerate}>Redesign your image</button>
       </div>
       <div className="gpt3__header-content">
         <div className="original">
@@ -64,7 +114,24 @@ export const Dream = () => {
         </div>
         <div className="generated">
           <h2>Uploaded Image</h2>
-          <div className='Uploaded'></div>
+          {(pictureRoute === '') &&
+            <div className='Uploaded'></div>
+          }
+          {(pictureRoute !== '') &&
+            <img src={pictureRoute} alt="ai" />
+          }
+          <div className="spinner-wrapper">
+            {(bLoadingFlag === true) &&
+              < ClipLoader
+                color='#ffffff'
+                loading={true}
+                cssOverride={true}
+                size={50}
+                aria-label="Loading Spinner"
+                data-testid="loader"
+              />
+            }
+          </div>
         </div>
       </div>
     </div>
