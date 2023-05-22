3e4f8cbb099ddf560ec610546baeeee55506ccf0
GloryDream413 <putnikdeveloperbg@gmail.com>
2023-05-22 10:56:30 +0900

    Image Flowing Function finished.

--- frontend/src/components/dashboard/Dashboard.jsx
+++ frontend/src/components/dashboard/Dashboard.jsx
@@ -3,6 +3,12 @@ import './dashboard.css';
 import aiorigin from '../../assets/aiorigin.png'
 import aigenerated from '../../assets/aigenerated.png'
 import { Link } from 'react-router-dom';
+import GalleryItem1 from '../galleryitem/Galleryitem1';
+import GalleryItem2 from '../galleryitem/Galleryitem2';
+import GalleryItem3 from '../galleryitem/Galleryitem3';
+import GalleryItem4 from '../galleryitem/Galleryitem4';
+import GalleryItem5 from '../galleryitem/Galleryitem5';
+import GalleryItem6 from '../galleryitem/Galleryitem6';
 
 export const Dashboard = () => {
   return (
@@ -22,6 +28,16 @@ export const Dashboard = () => {
           <h2>Generated Image</h2>
           <img src={aigenerated} alt="ai" />
         </div>
+      </div>
+      <div className="gpt3__header-content">
+        <GalleryItem1 />
+        <GalleryItem2 />
+        <GalleryItem3 />
+      </div>
+      <div className="gpt3__header-content">
+        <GalleryItem4 />
+        <GalleryItem5 />
+        <GalleryItem6 />
       </div>
     </div>
   );
